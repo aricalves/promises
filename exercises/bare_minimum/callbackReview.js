40bf7a5ef9bd34edf552a8139a324326d9e8955a
Aric Alves <aric.alves@yahoo.com>
2017-11-05 16:28:35 -0800

    Work through cbRev && promiseConstructor

--- exercises/bare_minimum/callbackReview.js
+++ exercises/bare_minimum/callbackReview.js
@@ -6,13 +6,23 @@ var fs = require('fs');
 var request = require('request');
 
 // This function should retrieve the first line of the file at `filePath`
-var pluckFirstLineFromFile = function (filePath) {
-  // TODO
+var pluckFirstLineFromFile = function (filePath, cb) {
+  fs.readFile(filePath, (err, data) => {
+    let firstLine = null;
+    if (data) firstLine = data.toString().split('\n')[0];
+    cb(err, firstLine);
+  });
 };
 
 // This function should retrieve the status code of a GET request to `url`
-var getStatusCode = function (url) {
-  // TODO
+var getStatusCode = function (url, cb) {
+  request(url, (err, res, body) => {
+    if (err) {
+      cb(err, null);
+    } else {
+      cb(null, res.statusCode);
+    }
+  });
 };
 
 // Export these functions so we can test them and reuse them in later exercises
